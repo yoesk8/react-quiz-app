0ad43546200b4c60572f86dde8e2d9fe20be8a16
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-08-08 19:52:32 +0000

    Created optionchosen state

--- my-app/src/components/Quiz.js
+++ my-app/src/components/Quiz.js
@@ -3,15 +3,18 @@ import { Questions } from '../helpers/QuestionBank';
 
 function Quiz() {
   const [currQuestion, setCurrentQuestion] = useState(0);
+  const [optionChosen, setOptionChosen] = useState("")
   return (
     <div className='Quiz'>
         <h1>{Questions[currQuestion].prompt}</h1>
         <div className='options'>
-          <button>{Questions[currQuestion].optionA}</button>
-          <button>{Questions[currQuestion].optionB}</button>
-          <button>{Questions[currQuestion].optionC}</button>
-          <button>{Questions[currQuestion].optionC}</button>
+          <button onClick={()=> setOptionChosen("A")}>{Questions[currQuestion].optionA}</button>
+          <button onClick={()=> setOptionChosen("B")}>{Questions[currQuestion].optionB}</button>
+          <button onClick={()=> setOptionChosen("C")}>{Questions[currQuestion].optionC}</button>
+          <button onClick={()=> setOptionChosen("D")}>{Questions[currQuestion].optionC}</button>
         </div>
+
+        <button>Next Question</button>
     </div>
   )
 }
--- my-app/src/helpers/QuestionBank.js
+++ my-app/src/helpers/QuestionBank.js
@@ -4,20 +4,23 @@ export const Questions = [
         optionA: "1",
         optionB: "2",
         optionC: "3",
-        optionD: "4"
+        optionD: "4",
+        answer: "D"
     },
     {
-        prompt: "What is 1 + 3",
+        prompt: "What is 1 + 2",
         optionA: "1",
         optionB: "2",
         optionC: "3",
-        optionD: "4"
+        optionD: "4",
+        answer: "C"
     },
     {
-        prompt: "What is 1 + 3",
+        prompt: "What is 1 + 1",
         optionA: "1",
         optionB: "2",
         optionC: "3",
-        optionD: "4"
+        optionD: "4",
+        answer: "B"
     }
 ]
