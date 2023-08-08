740652c9957b0a8132a215b7379a6680ad74e838
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-08-08 19:39:24 +0000

    Created questions and state for the quiz

--- my-app/src/components/Quiz.js
+++ my-app/src/components/Quiz.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Questions } from '../helpers/QuestionBank';
 
 function Quiz() {
+  const [currQuestion, setCurrentQuestion] = useState(0);
   return (
-    <div>
-        Quiz
+    <div className='Quiz'>
+        <h1>{Questions[currQuestion].prompt}</h1>
+        <div className='options'>
+          <button>{Questions[currQuestion].optionA}</button>
+          <button>{Questions[currQuestion].optionB}</button>
+          <button>{Questions[currQuestion].optionC}</button>
+          <button>{Questions[currQuestion].optionC}</button>
+        </div>
     </div>
   )
 }
---
+++ my-app/src/helpers/QuestionBank.js
@@ -0,0 +1,23 @@
+export const Questions = [
+    {
+        prompt: "What is 1 + 3",
+        optionA: "1",
+        optionB: "2",
+        optionC: "3",
+        optionD: "4"
+    },
+    {
+        prompt: "What is 1 + 3",
+        optionA: "1",
+        optionB: "2",
+        optionC: "3",
+        optionD: "4"
+    },
+    {
+        prompt: "What is 1 + 3",
+        optionA: "1",
+        optionB: "2",
+        optionC: "3",
+        optionD: "4"
+    }
+]
