e309388d3ba0684619244aacfa4a4292b5ab22b9
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-08-10 18:17:02 +0000

    Created finish game button and functionality

--- my-app/src/components/Quiz.js
+++ my-app/src/components/Quiz.js
@@ -4,16 +4,25 @@ import { QuizContext } from '../helpers/Contexts';
 
 function Quiz() {
 
-  const { score, setScore } = useContext(QuizContext)
+  const { score, setScore, setGameState } = useContext(QuizContext)
   const [currQuestion, setCurrentQuestion] = useState(0);
   const [optionChosen, setOptionChosen] = useState("");
   
   const nextQuestion = () => {
-        if (Questions[currQuestion].answer == optionChosen){
+        if (Questions[currQuestion].answer === optionChosen){
           setScore(score+1);
         }
-        alert(score)
+        console.log(score)
         setCurrentQuestion(currQuestion+1)
+
+  }
+
+  const finishQuiz = () => {
+    if (Questions[currQuestion].answer === optionChosen){
+      setScore(score+1);
+    }
+
+    setGameState("endscreen")
 
   }
   return (
@@ -23,10 +32,15 @@ function Quiz() {
           <button onClick={()=> setOptionChosen("A")}>{Questions[currQuestion].optionA}</button>
           <button onClick={()=> setOptionChosen("B")}>{Questions[currQuestion].optionB}</button>
           <button onClick={()=> setOptionChosen("C")}>{Questions[currQuestion].optionC}</button>
-          <button onClick={()=> setOptionChosen("D")}>{Questions[currQuestion].optionC}</button>
+          <button onClick={()=> setOptionChosen("D")}>{Questions[currQuestion].optionD}</button>
         </div>
 
-        <button onClick={nextQuestion}>Next Question</button>
+        {currQuestion == Questions.length - 1 ? (
+          <button onClick={finishQuiz}>Finish Quiz</button>
+        ): (
+          <button onClick={nextQuestion}>Next Question</button>
+        )}
+        
     </div>
   )
 }
